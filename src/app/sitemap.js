a9231318731eba3fd3ddf65542053963131e77a9
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-03-22 16:40:16 +0100

    Google Search Console validado

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://casaexpress.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://casaexpress.vercel.app/casas",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://casaexpress.vercel.app/auth",
+      lastModified: new Date(),
+    },
+  ]
+}
