55b985761e99449120a9fea456425a2cd0683eb8
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-04-15 13:41:54 +0100

    Update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,15 +1,15 @@
 export default function sitemap() {
   return [
     {
-      url: "https://casaexpress.vercel.app",
+      url: "https://casaexpress.fun",
       lastModified: new Date(),
     },
     {
-      url: "https://casaexpress.vercel.app/casas",
+      url: "https://casaexpress.fun/casas",
       lastModified: new Date(),
     },
     {
-      url: "https://casaexpress.vercel.app/auth",
+      url: "https://casaexpress.fun/auth",
       lastModified: new Date(),
     },
   ]
